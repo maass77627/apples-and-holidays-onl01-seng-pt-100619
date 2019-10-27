8ebfe441db00a50be4bdba84ee027f08f504156c
kelly r maass <kmaass77627@gmail.com>
2019-10-27 17:09:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,9 +42,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supplies|
       puts "#{holiday.to_s.split("_").map}|w|
       w.capitalize}.join("")}:#{supplies.join(", ")}"
-      supplies.each do |item|
-        puts "#{item}"
       end
     end
   end
-end
+
